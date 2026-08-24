70856a3ac70f546734aadb78229a6acedcb58be7
willytpfw <willytpfw@gmail.com>
2026-08-24 10:59:02 -0600

    Add new secret on HA

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 set -e
+
+#!/usr/bin/with-contenv bashio
+export JwtKey=$(bashio::config 'JwtKey')
+export JwtUsername=$(bashio::config 'JwtUsername')
+export JwtPassword=$(bashio::config 'JwtPassword')
 exec dotnet /app/WebJWT.dll --urls "http://0.0.0.0:8181"
+
